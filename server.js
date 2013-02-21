650c9c83ef39dbe737337daea0b15166ee073ae4
Rev Dan Catt <revdancatt@fluffykittens.com>
2013-02-21 16:30:34 +0000

    Read the guardian api key from the process.env instead

--- server.js
+++ server.js
@@ -15,31 +15,28 @@ colours.setTheme({
   error: 'red'
 });
 
-require('./control.js');
-
-//  check to see if we have been passed a guardian api key as a url parameter
-//  if we did then we need to write it to a file.
-
-//if ('guardian' in argv && 'api' in argv.guardian) {
-//    fs.writeFileSync('./guardian.key', argv.guardian.api, 'utf-8');
-//    console.log(('>> new guardian.key file written with: ' + argv.guardian.api).help);
-//}
-
-//  Check to see if the guardian.key file exists, if so we can read it
-//  in, otherwise we need to exit.
-if (fs.existsSync('./guardian.key')) {
-    
-    //  read in the file and start up the control thingy
-    control.init(fs.readFileSync('./guardian.key', 'utf-8'));
-
-} else {
-
-    //  start it up and send over null
-    console.log('>> Please visit the server in a web browser to enter your guardian API key.'.help);
-    control.init(null);
-
+//  ############################################################################
+//  Check to see if the guardian API key exists in the enviroment vars
+//  If not then we need to tell the user they need to set the var
+if (!('GUARDIANAPI' in process.env)) {
+    console.log('>> You need to set you Guardian API key in your enviroment vars.'.error.bold);
+    console.log('>> GUARDIANAPI=[your guardian api key]'.error);
+    console.log('>> See: https://groups.google.com/forum/?fromgroups=#!topic/nodejs/1CnOzd352JE for more information'.error);
+    process.exit(0);
 };
 
+if (!('MONGODB' in process.env)) {
+    console.log('>> You need to set you MONGODB connection thingy in your enviroment vars.'.error.bold);
+    console.log('>> MONGODB=[your mongodb connection thingy]'.error);
+    console.log('>> See: https://groups.google.com/forum/?fromgroups=#!topic/nodejs/1CnOzd352JE for more information'.error);
+    process.exit(0);
+};
+//  ############################################################################
+
+
+//  Now that we have safely got here we can carry on as though nothing is wrong
+require('./control.js');
+control.init(process.env.GUARDIANAPI);
 
 //  ############################################################################
 //  
@@ -61,33 +58,6 @@ http.createServer(function (request, response) {
         return;
     }
 
-
-    //  ########################################################################
-    //
-    //  check to see if we have been passed a guardian api key as a url parameter
-    //  if we did then we need to write it to a file, if the file doesn't already
-    //  exist
-    //
-    //  ########################################################################
-    if ('apikey' in url.parse(request.url, true).query && !fs.existsSync('./guardian.key')) {
-
-      //  grab the api key
-      //  TODO, validation here
-      var apikey = url.parse(request.url, true).query.apikey;
-      console.log(('>> new guardian.key file written with: ' + apikey).info);
-
-      //  Write the file to the file system 
-      fs.writeFileSync('./guardian.key', apikey, 'utf-8');
-
-      //  call the init on control again.
-      control.init(apikey);
-
-      //  redirect to the root url to drop the url parameter
-      response.writeHead(302, { 'Location': '/' });
-      response.end();
-    }
-
-
     //  ########################################################################
     //
     //  THIS IS THE ONE WE REALLY CARE ABOUT, as it will...
@@ -96,19 +66,10 @@ http.createServer(function (request, response) {
     //  ########################################################################
     request.on('end', function () {
 
-        //  If we don't have a guardian api key then we need to
-        //  ask the user for one. Using a VERY BAD FORM!!!
-        //  WHICH WE WILL FIX :)
-        if (control.guardian.key == null) {
-          response.writeHead(200, {'Content-Type': 'text/html'});
-          response.write('You need to enter a Guardian API key<br />');
-          response.write('<form><input name="apikey" type="text" /></form><br />');
-          response.end();
-        } else {
-          control.count++;
-          response.writeHead(200, {'Content-Type': 'text/plain'});
-          response.end('Hello world : ' + control.count.toString());
-        }
+        control.count++;
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('Hello brave new world : ' + control.count.toString());
+
     });
 
 }).listen(1337);
